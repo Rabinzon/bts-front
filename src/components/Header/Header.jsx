77f5e22bf2237f21a86ed312b54b040c7293f309
rabinzon <ildarrazin@gmail.com>
2017-03-16 18:06:17 +0300

    feat(header): add youtube link

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import Isvg from 'react-inlinesvg';
 import Container from '../Container';
-import img from '../../assets/microphone.svg';
-var Isvg = require('react-inlinesvg');
+import microphone from '../../assets/microphone.svg';
+import vk from '../../assets/vk.svg';
+import youtube from '../../assets/youtube.svg';
+
 import styles from './Header.css';
 
 
@@ -13,12 +16,20 @@ class Header extends React.Component {
 					<Container>
 						
 						<div className={styles.content}>
-							<Isvg className={styles.icon} src={img}></Isvg>
+							<Isvg className={styles.icon} src={microphone}></Isvg>
 							<a href='/' className={styles.logo}>Безнең тәҗрибә</a>
 							<menu className={styles.menu}>
 								<a href='/' className={[styles.link, styles.active].join(' ')}>Фильмнар</a>
 								<a href='/' className={styles.link}>Безнең хакта</a>
 							</menu>
+							<div className={styles.social}>
+								<a className={styles.socialLink} href='https://www.youtube.com/channel/UCDCaB11-PTI3dZyTBG7CylA'>
+									<Isvg className={styles.youtube} src={youtube}></Isvg>
+								</a>
+								<a className={styles.socialLink} href='https://vk.com/beztejribe'>
+									<Isvg className={styles.vk} src={vk}></Isvg>
+								</a>
+							</div>
 						</div>
 					</Container>
 				</div>
