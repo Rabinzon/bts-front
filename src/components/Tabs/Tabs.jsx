0d4841a87a62fc9224ed6b2b331c9f6438b22b76
rabinzon <ildarrazin@gmail.com>
2017-03-17 09:58:32 +0300

    feat(tabs): add active state

--- src/components/Tabs/Tabs.jsx
+++ src/components/Tabs/Tabs.jsx
@@ -2,12 +2,21 @@ import React from 'react';
 import styles from './Tabs.css';
 
 class Tabs extends React.Component {
+	state = {
+		active: 0
+	}
+	
+	onClick = active => () => {
+		this.setState({active})
+	}
+
 	render() {
 		const {list} = this.props;
+		const {active} = this.state;
 		
 		return (
 			<div className={styles.tabs}>
-				{list.map((text, index) => <a href="/" className={index === 0 ? styles.active : styles.tab} key={index}>{text}</a>)}
+				{list.map((text, index) => <a onClick={this.onClick(index)} className={index === active ? styles.active : styles.tab} key={index}>{text}</a>)}
 			</div>
 		);
 	}
