bbb13a43c78a235cba46790f467a41335fdebc7a
Ildar Gilfanov <ildarrazin@gmail.com>
2017-07-04 10:40:37 +0300

    Feat/webpack (#5)
    * feat: optimaze webpack, add dev/prod building
    
    * feat(npm): add scripts
    
    * fix(Dockerfile): add new command
    
    * fix(server): port from env
    
    * chore: update .gitignore

---
+++ webpack/common.config.js
@@ -0,0 +1,74 @@
+const webpack = require('webpack');
+const HappyPack = require('HappyPack');
+
+const env = process.env;
+const production = env.NODE_ENV === 'production';
+
+const envs = {
+	env: env
+};
+
+const stats = {
+	colors: false,
+	hash: false,
+	version: false,
+	timings: true,
+	assets: false,
+	chunks: false,
+	modules: false,
+	reasons: false,
+	children: false,
+	source: false,
+	errors: false,
+	errorDetails: false,
+	warnings: false,
+	publicPath: false
+};
+
+const happyPackOptions = {
+	verbose: false,
+	threads: 4,
+	loaders: [{
+		loader: 'babel-loader',
+		options : {
+			presets : [
+				"react",
+				"es2015",
+				"babel-preset-stage-0"
+			]
+		}
+	}],
+};
+
+const UglifyJs = () => {
+	if (production) {
+		return new webpack.optimize.UglifyJsPlugin({
+			beautify: false,
+			comments: false,
+			compress: {
+				sequences: true,
+				booleans: true,
+				loops: true,
+				unused: true,
+				warnings: false,
+				drop_console: true,
+				unsafe: true
+			}
+		});
+	}
+	return null;
+};
+
+const plugins =  [
+	UglifyJs(),
+	new HappyPack(happyPackOptions),
+	new webpack.NoEmitOnErrorsPlugin(),
+	new webpack.DefinePlugin(envs),
+	new webpack.optimize.DedupePlugin(),
+	new webpack.optimize.OccurrenceOrderPlugin()
+].filter(t => t);
+
+module.exports = {
+	plugins,
+	stats
+};
